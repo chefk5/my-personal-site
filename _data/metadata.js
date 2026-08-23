204a549b7b251c8b4e3bc756f0b4b11491035c2e
Khaledc <kncharkie@gmail.com>
2026-08-23 20:59:59 +0300

    Cleanup and first post

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "Eleventy Base Blog v9",
+	title: "Khaled Charkie",
 	url: "https://example.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Thoughts beyond the code",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
-	}
-}
+		name: "Khaled Charkie",
+		email: "khaledncharkie@gmail.com",
+		url: "https://example.com/about-me/",
+	},
+};
